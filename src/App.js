75c0d2c08ecc2139178dd868afab68bf49d8442f
Harlan Heath <HarlanDHeath@gmail.com>
2018-08-27 17:06:57 -0500

    Nav bar update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import Routes from './Routes';
-import './App.css';
+import React, { Component } from "react";
+import Routes from "./Routes";
+import Nav from "./components/Nav/Nav";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
         <Routes />
       </div>
     );
