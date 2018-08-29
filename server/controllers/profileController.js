53b1fa9bddc973102cd4be73a877f197df93d9bf
robert hunnicutt <robhunn74@gmail.com>
2018-08-29 16:51:21 -0500

    landing page is hooked up to db

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,6 +1,5 @@
 const updateProfile = (req, res) => {
     const { name, photo, city, state} = req.body;
-console.log(req.body)
     req.app.get('db').userProfile.updateProfile([name, photo, city, state, req.user.user_id])
 
       .then(collections => res.status(200).send(collections))
@@ -10,6 +9,20 @@ console.log(req.body)
       });
   }
 
+  const getUser = (req,res) =>{
+    let user_id = req.user.user_id;
+    req.app.get('db').userProfile.getUser(user_id)
+    .then(payload => res.status(200).json(payload))
+    .catch(err => {
+      console.log(err)
+      res.status(500);
+    });
+  }
+
+
+
+
   module.exports = {
-    updateProfile
+    updateProfile,
+    getUser
   }
