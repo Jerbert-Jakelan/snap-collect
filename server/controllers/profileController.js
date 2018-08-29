aa27d953c000bb7380a4a95b09448b988fd6d2e0
blermyjake <jeremy73120@gmail.com>
2018-08-29 13:30:21 -0500

    update working

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,10 +1,14 @@
 const updateProfile = (req, res) => {
-    // need to write to update profile pic
+    const { name, photo, city, state} = req.body;
+console.log(req.body)
+    req.app.get('db').userProfile.updateProfile([name, photo, city, state, req.user.user_id])
+
+      .then(collections => res.status(200).send(collections))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+      });
   }
-
-
-
-
 
   module.exports = {
     updateProfile
