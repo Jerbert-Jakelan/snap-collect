d17c9ced73cebef3b2c44c604f174a86295823e2
Jake Wymer <jakeandrewwymer@gmail.com>
2018-08-29 15:44:42 -0500

    write integration tests for cards

--- cypress/integration/cards-stub.spec.js
+++ cypress/integration/cards-stub.spec.js
@@ -3,12 +3,27 @@ describe('Cards Stub Test', () => {
     cy.server();
     cy.route({
       method: 'GET',
-      url: '/api/cards',
+      url: '/api/cards/3',
       response: "fixture:cards"
     });
-    cy.visit('/');
+    cy.visit('/#/collection/3');
     cy.get('[data-cy-cards]')
       .should('exist')
       .should('have.length', 3);
+  });
+
+  it('Allows Name Input', () => {
+    cy.visit('/#/collection/3');
+    cy.selectAndType('[data-cy-card-name-input]', 'Jakey Wymes');
+  });
+
+  it('Allows Team Input', () => {
+    cy.visit('/#/collection/3');
+    cy.selectAndType('[data-cy-card-team-input]', 'Texas Rangers');
+  });
+
+  it('Allows Year Input', () => {
+    cy.visit('/#/collection/3');
+    cy.selectAndType('[data-cy-card-year-input]', '1994');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('selectAndType', (selector, value) => {
+  cy.get(selector)
+    .type(value)
+    .should('have.value', value);
+});
