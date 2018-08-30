857ab91b8c4c17ac3f14ae6d434f5da5513861e5
blermyjake <jeremy73120@gmail.com>
2018-08-30 14:31:41 -0500

    styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import "./Login.css";
 
 export default class Login extends Component {
@@ -14,13 +13,38 @@ export default class Login extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Snap Collect</h1>
-        <h2>Logging in gives you acccess to the goods</h2>
-        <li className="list">Use this field for point 1</li>
-        <li className="list">Obviously this is where point 2 sits</li>
-        <li className="list">Bob Ross is a God</li>
-        <a href={process.env.REACT_APP_LOGIN}>Login</a>
+      <div className="landingMainDiv">
+        <div className="landingDiv2">
+          <h4>Welcome to</h4>
+          <h1 className="appName">Snap Collect</h1>
+          <div className="imgDiv">
+            <img
+              className="loginImg"
+              src={require("./cam256x256.png")}
+              alt=""
+            />
+          </div>
+          <div className="appDescDiv">
+            <p className="appDescription">
+              The app that lets you scan your collections
+            </p>
+          </div>
+
+          {/* <li className="list">Use this field for point 1</li>
+          <li className="list">Obviously this is where point 2 sits</li>
+          <li className="list">Bob Ross is a God</li> */}
+
+          <hr className="hr" />
+          <div className="loginSeg">
+            <div className="loginSegText">
+              <h4 className="clickHereText">Click here to sign up</h4>
+              <h4 className="loginText">or log in</h4>
+              <a href={process.env.REACT_APP_LOGIN}>
+                <button classname="buttonLanding">Get Started</button>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
