8bb42f12907e7aa81549ad2c3228d2b656058f6f
Harlan Heath <HarlanDHeath@gmail.com>
2018-09-03 11:09:07 -0500

    public component create and updated nav

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import {Switch, Route} from 'react-router-dom';
-import CollectionDetail from './components/CollectionDetail/CollectionDetail';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import CollectionDetail from "./components/CollectionDetail/CollectionDetail";
+import PubCollections from "./components/PubCollections/PubCollections";
 import Landing from "./components/Landing/Landing";
 import Login from "./components/Login/Login";
 
@@ -10,7 +11,7 @@ class Routes extends Component {
       <Switch>
         <Route path="/" component={Login} exact />
         <Route path="/dashboard" component={Landing} exact />
-
+        <Route path="/publiccollections" component={PubCollections} />
         <Route path="/collection/:collection_id" component={CollectionDetail} />
       </Switch>
     );
