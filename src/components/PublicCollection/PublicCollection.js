c5206d7b90d62e9522d708f7149a7028bef01933
robert hunnicutt <robhunn74@gmail.com>
2018-09-03 12:53:58 -0500

    publicCollection component set up

--- src/components/PublicCollection/PublicCollection.js
+++ src/components/PublicCollection/PublicCollection.js
@@ -5,28 +5,43 @@ import {Card} from 'reactstrap';
 
 
 class PublicCollection extends Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
-            cards: [],
-            input: ""
-          };
-    }
-    componentDidMount() {
-        this.getCards();
+          users:[],
+          collection:[],
+          input:''
+        }
+        this.updateCollections = this.updateCollections.bind(this)
       }
-    
-      getCards = () => {
-        axios
-          .get(`/api/cards/${this.props.match.params.collection_id}`)
-          .then(cards => this.setState({ cards: cards.data }))
-          .catch(err => console.log(err));
-      }
-      updateCards = cards => {
-        this.setState({ cards: cards });
+
+      componentDidMount = async() => {
+        this.getCollections();
+        this.getUsers();
       };
 
-    handleInput(val) {
+      getUsers = () => {
+        axios.get('/api/getProfile')
+        .then(res => {
+          this.setState({
+            users:res.data
+          })
+        })
+      }
+    
+      getCollections = () => {
+        axios.get('/api/collections').then(payload =>{
+          this.setState({
+              collection: payload.data
+          })
+        })
+        }
+
+      updateCollections = collection => {
+        this.setState({collection});
+      }
+
+    handleInput = (val) => {
         this.setState({
           input: val.toLowerCase()
         });
@@ -34,18 +49,14 @@ class PublicCollection extends Component{
     
       render() {
         const { input } = this.state;
-        let cardSearch = this.state.cards
+        let collectionSearch = this.state.collection
           .filter(
             e =>
               e.name.toLowerCase().includes(input) ||
-              e.team.toLowerCase().includes(input) ||
-              e.year.toLowerCase().includes(input)
+              e.description.toLowerCase().includes(input)
           )
           .map((e, i) => {
-            return <Card updateCards={this.updateCards} key={e.card_id} card={e} />;
-            // <div key={i}>
-            //   <div>{e.name}</div>
-            // </div>;
+            return <Card index={i} updateCollections={this.updateCollections} key={e.collection_id} card={e} />;
           })
     
         return (
@@ -55,7 +66,7 @@ class PublicCollection extends Component{
                 placeholder="Search"
                 onChange={event => this.handleInput(event.target.value)}
                 />
-            <div className="card-wrapper"> {cardSearch} </div>
+            <div className="card-wrapper"> {collectionSearch} </div>
         </div>
     )
 }
