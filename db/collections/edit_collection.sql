ce22756f6e41cd145fe81e86dfa42d4dcc7acdd9
Jake Wymer <jakeandrewwymer@gmail.com>
2018-09-03 16:44:36 -0500

    edit collection feature

--- db/collections/edit_collection.sql
+++ db/collections/edit_collection.sql
@@ -1,6 +1,6 @@
 UPDATE collections
-SET name = $2
+SET name = $2, category_id = $3, description = $4, private = $5
 WHERE collection_id = $1;
 
 SELECT * FROM collections
-WHERE user_id = $3;
+WHERE user_id = $6;
