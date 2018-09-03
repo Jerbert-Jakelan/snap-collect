83652329805bd38b8ae7fdab0274bae2fa42f128
Jake Wymer <jakeandrewwymer@gmail.com>
2018-09-03 10:13:57 -0500

    delete card images from s3

--- db/cards/create_card.sql
+++ db/cards/create_card.sql
@@ -1,5 +1,5 @@
-INSERT INTO cards(name, team, year, image, collection_id)
-VALUES($1, $2, $3, $4, $5);
+INSERT INTO cards(name, team, year, image, collection_id, aws_key)
+VALUES($1, $2, $3, $4, $5, $6);
 
 SELECT * FROM cards
 WHERE collection_id = $5;
