9ec10a638bda8d8c8c67ab2c6e34e5e050597a99
blermyjake <jeremy73120@gmail.com>
2018-09-04 16:28:05 -0500

    st

--- src/components/Landing/Collections/Collections.js
+++ src/components/Landing/Collections/Collections.js
@@ -1,47 +1,76 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
-import { Card, CardHeader, CardBody, CardFooter, CardText, Button } from "reactstrap";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardText,
+  Button
+} from "reactstrap";
 import { connect } from "react-redux";
 import Switch from "react-switch";
-import axios from 'axios';
+import axios from "axios";
 import { selectCollection } from "../../../ducks/reducer";
 import "./Collections.css";
 import EditCollection from "../EditCollection/EditCollection";
 
 class Collections extends Component {
-
   state = {
     checked: true
-  }
+  };
 
   componentDidMount() {
-    this.setState({checked: !this.props.priv});
+    this.setState({ checked: !this.props.priv });
   }
 
   handleChange = checked => {
     this.setState({ checked }, async () => {
-      let collections = await axios.put(`/api/collections/${this.props.collId}/private`, {private: !this.state.checked});
+      let collections = await axios.put(
+        `/api/collections/${this.props.collId}/private`,
+        { private: !this.state.checked }
+      );
       this.props.updateCollections(collections.data);
     });
-  }
+  };
 
   render() {
-    let toggleSwitch = this.props.match.path !== '/PublicCollection' ? 
-      <Switch onChange={this.handleChange} checked={this.state.checked} id="normal-switch" /> :
-      null
+    let toggleSwitch =
+      this.props.match.path !== "/PublicCollection" ? (
+        <Switch
+          // className="toggleSwitch"
+          onChange={this.handleChange}
+          checked={this.state.checked}
+          id="normal-switch"
+        />
+      ) : null;
 
     return (
       <div data-cy-collections className="cardDisplay">
         <Card>
-          <CardHeader>
-            {this.props.name}
-            {toggleSwitch}
-          </CardHeader>
-          {this.props.proPic ? 
-          <img height={50} width={50} style={{borderRadius:"50%"}}src={this.props.proPic} alt="userpic"/> : null}
-          <p>{this.props.userName ? this.props.userName : null}</p>
-          <p>{this.props.city ? this.props.city : null}</p>
-          <p>{this.props.state ? this.props.state : null}</p>
+          <div className="toggleSwitch">
+            <CardHeader className="cardHeadPubCol">
+              {this.props.name}
+              {toggleSwitch}
+            </CardHeader>
+          </div>
+          <div className="pubColFullCard">
+            {this.props.proPic ? (
+              <img
+                className="avatarPubPic"
+                // height={50}
+                // width={50}
+                // style={{ borderRadius: "50%" }}
+                src={this.props.proPic}
+                alt="userpic"
+              />
+            ) : null}
+            <div className="pubColUserDetails">
+              <p>{this.props.userName ? this.props.userName : null}</p>
+              <p>{this.props.city ? this.props.city : null}</p>
+              <p>{this.props.state ? this.props.state : null}</p>
+            </div>
+          </div>
           <CardBody>
             <div className="collections">
               <Link to={`/collection/${this.props.collId}`}>
@@ -57,13 +86,14 @@ class Collections extends Component {
             <CardText>{this.props.description}</CardText>
           </CardBody>
           <CardFooter>
-            <EditCollection 
+            <EditCollection
               key={this.props.collection_id}
               collId={this.props.collId}
               image={this.props.collection_pic}
               name={this.props.name}
               description={this.props.description}
-              updateCollections={this.props.updateCollections}/>
+              updateCollections={this.props.updateCollections}
+            />
           </CardFooter>
         </Card>
       </div>
@@ -72,7 +102,9 @@ class Collections extends Component {
 }
 const mapStateToProps = state => state;
 
-export default withRouter(connect(
-  mapStateToProps,
-  { selectCollection }
-)(Collections));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { selectCollection }
+  )(Collections)
+);
