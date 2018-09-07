9594286ba134312ad998df71e26d77c0d78324c5
Jake Wymer <jakeandrewwymer@gmail.com>
2018-09-07 09:59:10 -0500

    add protected route to dashboard

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -66,15 +66,19 @@ const updateProfile = (req, res) => {
 };
 
 const getUser = (req, res) => {
-  let user_id = req.user.user_id;
-  req.app
-    .get("db")
-    .userProfile.getUser(user_id)
-    .then(payload => res.status(200).json(payload))
-    .catch(err => {
-      console.log(err);
-      res.status(500);
-    });
+  if(!req.user) {
+    res.send({message: 'Please log in'});
+  } else {
+    let user_id = req.user.user_id;
+    req.app
+      .get("db")
+      .userProfile.getUser(user_id)
+      .then(payload => res.status(200).json(payload))
+      .catch(err => {
+        console.log(err);
+        res.status(500);
+      });
+  }
 };
 
 const getAllUsers = (req, res) => {
